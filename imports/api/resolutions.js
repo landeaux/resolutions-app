900cb0d0a18ba76675aa79ee5c7ea119dd83592e
landeaux <pluckedowl@gmail.com>
2018-12-31 10:50:18 -0800

    Refactor project

--- imports/api/resolutions.js
+++ imports/api/resolutions.js
@@ -1,1 +1,3 @@
+import { Mongo } from 'meteor/mongo';
 
+export const Resolutions = new Mongo.Collection('resolutions');
--- server/main.js
+++ server/main.js
@@ -1,48 +1,10 @@
 import { Meteor } from 'meteor/meteor';
-import { Mongo } from 'meteor/mongo';
-import { check } from 'meteor/check';
+import { Resolutions } from '../imports/api/resolutions';
+
+import './methods.js';
 
 Meteor.startup(() => {
   // code to run on server at startup
-  const Resolutions = new Mongo.Collection('resolutions');
-
-  Meteor.methods({
-    addResolution: (title) => {
-      check(title, String);
-      Resolutions.insert({
-        title,
-        createdAt: new Date(),
-        owner: Meteor.userId(),
-      });
-    },
-    deleteResolution: (id) => {
-      check(id, String);
-      const res = Resolutions.findOne(id);
-      if (res.owner !== Meteor.userId()) {
-        throw new Meteor.Error('not-authorized');
-      }
-      Resolutions.remove(id);
-    },
-    updateResolution: (id, checked) => {
-      check(id, String);
-      check(checked, Boolean);
-      const res = Resolutions.findOne(id);
-      if (res.owner !== Meteor.userId()) {
-        throw new Meteor.Error('not-authorized');
-      }
-      Resolutions.update(id, { $set: { checked } });
-    },
-    setPrivate: (id, priv) => {
-      check(id, String);
-      check(priv, Boolean);
-      const res = Resolutions.findOne(id);
-      if (res.owner !== Meteor.userId()) {
-        throw new Meteor.Error('not-authorized');
-      }
-
-      Resolutions.update(id, { $set: { private: priv } });
-    },
-  });
 
   Meteor.publish('resolutions', function() {
     return Resolutions.find({
